8aedd573f6d5b73a727522ae845a1fb1453ce0ec
Andrew Leonard <andrewleonard1693@gmail.com>
2018-04-16 00:23:49 -0400

    added event handler to back buttons for actionbar within the game

--- app/src/main/java/com/example/aj/chess_android/NewGameActivity.java
+++ app/src/main/java/com/example/aj/chess_android/NewGameActivity.java
@@ -1,10 +1,14 @@
 package com.example.aj.chess_android;
 
+import android.content.DialogInterface;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.view.View;
 
 public class NewGameActivity extends AppCompatActivity {
@@ -17,6 +21,10 @@ public class NewGameActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
         setTitle("Good Luck!");
 
+        //handle the back button to make sure that the user wants to go back
+        //pressing the back button will not save the currently running game
+
+
 //        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
 //        fab.setOnClickListener(new View.OnClickListener() {
 //            @Override
@@ -27,5 +35,48 @@ public class NewGameActivity extends AppCompatActivity {
 //        });
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
+
+    @Override
+    public void onBackPressed() {
+        //Display alert message when back button has been pressed
+        backButtonHandler();
+        return;
+    }
+
+    public void backButtonHandler() {
+        AlertDialog.Builder alertDialog = new AlertDialog.Builder(
+                NewGameActivity.this);
+        // Setting Dialog Title
+        alertDialog.setTitle("Leave?");
+        // Setting Dialog Message
+        alertDialog.setMessage("Are you sure you want to leave the game? It won't be saved.");
+        // Setting Positive "Yes" Button
+        alertDialog.setPositiveButton("Yes",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        finish();
+                    }
+                });
+        // Setting Negative "NO" Button
+        alertDialog.setNegativeButton("No",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        // Write your code here to invoke NO event
+                        dialog.cancel();
+                    }
+                });
+        // Showing Alert Message
+        alertDialog.show();
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            onBackPressed();
+            return true;
+        }
+        return false;
+    }
+    //handle the back button to make sure that the user wants to go back
+    //pressing the back button will not save the currently running game
 
 }
