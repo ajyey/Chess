946c893c9ec357581bacdc0bc1652642a6b7282c
aj <andrewleonard1693@gmail.com>
2018-04-17 04:51:24 -0400

    added checkmate handler function declaration

--- app/src/main/java/com/example/aj/chess_android/NewGameActivity.java
+++ app/src/main/java/com/example/aj/chess_android/NewGameActivity.java
@@ -390,10 +390,13 @@ public class NewGameActivity extends AppCompatActivity {
         }
     }
     //TODO:
-        //implement the dialog for the checkmate
+        //implement the dialog to ask the user if they want save the game and provide a text input for a name for the game
     public void checkmateHandler(){
+        TextView textView = (TextView) findViewById(R.id.gameResult);
+
         if(game.checkmate()){
-            String winner = (game.isWhitesTurn()) ? "White wins": "Black wins";
+            String winner = (game.isWhitesTurn()) ? "White wins!": "Black wins!";
+            textView.setText(winner);
         }
     }
 
